c2480d706fcadf42fba81ea9d415b444e90243f7
bjornjohansen <bjornjohansen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-06 17:58:17 +0000

    Version 0.2 Near complete rewrite. Added options panel in admin Added option to lazy load post thumbnails Skipped the lazy loading in feeds
    git-svn-id: https://plugins.svn.wordpress.org/bj-lazy-load/trunk@471665 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bj-lazyload.php
+++ bj-lazyload.php
@@ -3,7 +3,7 @@
 Plugin Name: BJ Lazy Load
 Plugin URI: http://wordpress.org/extend/plugins/bj-lazy-load/
 Description: Lazy image loading makes your site load faster and saves bandwidth.
-Version: 0.1
+Version: 0.2
 Author: Bjørn Johansen
 Author URI: http://twitter.com/bjornjohansen
 License: GPL2
@@ -28,7 +28,25 @@ License: GPL2
 
 class BJLL {
 
-	function enqueue_scripts() {
+	private $_placeholder_url;
+
+	function __construct() {
+		
+		$this->_placeholder_url = plugins_url('/img/placeholder.gif', __FILE__);
+	
+		add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
+
+		add_action( 'wp_ajax_BJLL_get_images', array($this, 'get_images_json') );
+		add_action( 'wp_ajax_nopriv_BJLL_get_images', array($this, 'get_images_json') );
+
+		add_filter('the_content', array($this, 'filter_post_images'), 200);
+		
+		if (intval(get_option('bjll_filter_post_thumbnails', 1))) {
+			add_filter( 'post_thumbnail_html', array($this, 'filter_post_thumbnail_html'), 10 );
+		}
+	}
+
+	public function enqueue_scripts() {
 		wp_enqueue_script('JAIL', plugins_url('/js/jail.min.js', __FILE__), array('jquery'), '0.9.7', true);
 		
 		wp_enqueue_script( 'BJLL', plugins_url('/js/bjll.js', __FILE__), array( 'jquery', 'JAIL' ), '0.1', true );
@@ -38,14 +56,12 @@ class BJLL {
 		*/
 	} 
 
-	function get_images_json() {
+	public function get_images_json() {
 		echo json_encode($_POST['attachmentIDs']);
 		exit;
 	}
 	
-	function filter_post_images($content) {
-	
-		$placeholder_url = plugins_url('/img/placeholder.gif', __FILE__);
+	public function filter_post_images($content) {
 	
 		$matches = array();
 		preg_match_all('/<img\s+.*?>/', $content, $matches);
@@ -54,19 +70,8 @@ class BJLL {
 		$replace = array();
 		
 		foreach ($matches[0] as $imgHTML) {
-			$replaceHTML = $imgHTML;
 			
-			// replace the src and add the data-href attribute
-			$replaceHTML = preg_replace( '/<img(.*?)src=/i', '<img$1src="'.$placeholder_url.'" data-href=', $replaceHTML );
-			
-			// add the lazy class to the img element
-			if (preg_match('/class="/i', $replaceHTML)) {
-				$replaceHTML = preg_replace('/class="(.*?)"/i', ' class="lazy $1"', $replaceHTML);
-			} else {
-				$replaceHTML = preg_replace('/<img/i', '<img class="lazy"', $replaceHTML);
-			}
-			
-			$replaceHTML .= '<noscript>' . $imgHTML . '</noscript>';
+			$replaceHTML = $this->_get_placeholder_html($imgHTML);
 			
 			array_push($search, $imgHTML);
 			array_push($replace, $replaceHTML);
@@ -77,12 +82,104 @@ class BJLL {
 		return $content;
 	}
 	
+	public function filter_post_thumbnail_html( $html ) {
+
+	  $html = $this->_get_placeholder_html($html);
+	  
+	  return $html;
+
+	}
+	
+	protected function _get_placeholder_html ($html) {
+	
+		$orig_html = $html;
+			
+		// replace the src and add the data-href attribute
+		$html = preg_replace( '/<img(.*?)src=/i', '<img$1src="'.$this->_placeholder_url.'" data-href=', $html );
+		
+		// add the lazy class to the img element
+		if (preg_match('/class="/i', $html)) {
+			$html = preg_replace('/class="(.*?)"/i', ' class="lazy $1"', $html);
+		} else {
+			$html = preg_replace('/<img/i', '<img class="lazy"', $html);
+		}
+		
+		$html .= '<noscript>' . $orig_html . '</noscript>';
+	
+		return $html;
+	}
+	
 }
 
-add_action('wp_enqueue_scripts', array('BJLL', 'enqueue_scripts'));
+class BJLL_Admin {
 
-add_action( 'wp_ajax_BJLL_get_images', array('BJLL', 'get_images_json') );
-add_action( 'wp_ajax_nopriv_BJLL_get_images', array('BJLL', 'get_images_json') );
+	function __construct () {
+		add_action('admin_menu', array($this, 'plugin_menu'));
+		add_action( 'admin_init', array($this, 'register_settings'));
+	}
 
-add_filter('the_content', array('BJLL', 'filter_post_images'), 200);
+	function plugin_menu() {
+		add_options_page('BJ Lazy Load', 'BJ Lazy Load', 'manage_options', 'bjll', array($this, 'plugin_options_page'));
+	}
+	
+	function register_settings() {
+		//register_setting( $option_group, $option_name, $sanitize_callback );
+		register_setting( 'bjll_options', 'bjll_filter_post_thumbnails', 'intval' );
+
+		//add_settings_section( $id, $title, $callback, $page );
+		add_settings_section('bjll_general', __('General'), array('BJLL_Admin','settings_section_general'), 'bjll');
+		
+		//add_settings_field( $id, $title, $callback, $page, $section, $args );
+		add_settings_field('bjll_filter_post_thumbnails', __('Lazy load post thumbnails'), array('BJLL_Admin', 'setting_field_filter_post_thumbnails'), 'bjll', 'bjll_general');
+
+		
+	}
+	
+	function settings_section_general() {
+	}
+	
+	function setting_field_filter_post_thumbnails() {
+
+		$checked = '';
+		if (intval(get_option('bjll_filter_post_thumbnails', 1))) {
+			$checked = ' checked="checked"';
+		}
+		
+		echo '<input id="bjll_filter_post_thumbnails" name="bjll_filter_post_thumbnails" type="checkbox" value="1" ' . $checked . ' />';
+
+	}
+
+	function plugin_options_page() {
+		if (!current_user_can('manage_options'))  {
+			wp_die( __('You do not have sufficient permissions to access this page.') );
+		}
+		?>
+		<div class="wrap">
+			<h2>BJ Lazy Load <?php _e('Settings'); ?></h2>
+			<p><?php _e('More settings will be available in the near future.'); ?></p>
+			<form method="post" action="options.php">
+				<?php settings_fields('bjll_options'); ?>
+				<?php do_settings_sections('bjll'); ?>
+				
+				<p class="submit">
+				<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+				</p>
+			</form>
+		</div>
+		<?php
+	}
+
+}
+
+/*
+is_admin() will return true when trying to make an ajax request
+if (!is_admin() && !is_feed()) {
+*/
+if (!is_feed()) {
+	new BJLL;
+}
+
+if (is_admin()) {
+	new BJLL_Admin;
+}
 
--- readme.txt
+++ readme.txt
@@ -5,18 +5,18 @@ Tags: images, lazy loading, jquery, javascript, optimize, performance, bandwidth
 Author URI: http://twitter.com/bjornjohansen
 Requires at least: 3.2
 Tested up to: 3.3
-Stable tag: 0.1
+Stable tag: 0.2
 
 Lazy image loading makes your site load faster and saves bandwidth. Uses jQuery and degrades gracefully for non-js users.
 
 == Description ==
 Lazy image loading makes your site load faster and saves bandwidth.
 
-This plugin replaces all your post images with a placeholder and loads images as they enter the browser window.
+This plugin replaces all your post images and post thumbnails with a placeholder and loads images as they enter the browser window when the visitor scrolls the page.
 
 Non-javascript visitors gets the original img element in noscript.
 
-Contains [JqueryAsynchImageLoader Plugin for jQuery by Sebastiano Armeli-Battana](http://www.sebastianoarmelibattana.com/projects/jail)
+Includes [JqueryAsynchImageLoader Plugin for jQuery by Sebastiano Armeli-Battana](http://www.sebastianoarmelibattana.com/projects/jail) for the real magic.
 
 = Coming soon =
 * More options like defining a threshold, loading effects, custom placeholder etc.
@@ -43,6 +43,11 @@ Your HTML should be standards compliant.
 Check your HTML source or see the magic at work in FireBug or similar.
 
 == Changelog ==
+
+= Version 0.2 =
+* Added options panel in admin
+* Added option to lazy load post thumbnails
+* Skipped the lazy loading in feeds
 
 = Version 0.1 =
 * Released 2011-12-05
